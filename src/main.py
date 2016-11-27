42b2c11992a6c11b550508312c7da6224e379e67
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2016-11-27 11:58:32 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from gui import initialisation_of_the_window
+
+
+string_to_convert = None
+string_converted = None
+src_base = None
+dest_base = None
+
+
+def main():
+    root = initialisation_of_the_window()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
