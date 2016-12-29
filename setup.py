370be9b2ce9e1321a39f5c720fb8cec5da5b6784
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2016-12-29 09:45:00 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='mypackage',
+    packages=['mypackage'],  # this must be the same as the name above
+    version='0.1',
+    description='A random test lib',
+    author='Peter Downs',
+    author_email='peterldowns@gmail.com',
+    url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+    classifiers=[],
+)
