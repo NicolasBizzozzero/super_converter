f2838b1d9d4a5683f1b2223bded7513d65d6c388
NicolasBi <nicolasbizzozzero@gmail.com>
2016-12-31 19:44:30 +0100

    did stuff

--- env_super_converter/super_converter/super_converter/locale/generate_pot.cmd
+++
@@ -1,1 +0,0 @@
-py -3.5 C:\Users\Nicolas\AppData\Local\Programs\Python\Python35-32\Tools\i18n\pygettext.py -d translation_gui ../lib/*.py
--- setup.py
+++ setup.py
@@ -1,14 +1,105 @@
-from distutils.core import setup
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+#import ez_setup
+# ez_setup.use_setuptools()
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+import super_converter
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='mypackage',
-    packages=['mypackage'],  # this must be the same as the name above
-    version='0.1',
-    description='A random test lib',
-    author='Peter Downs',
-    author_email='peterldowns@gmail.com',
-    url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
-    # I'll explain this in a second
-    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
-    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
-    classifiers=[],
+    name='Super-Converter',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=super_converter.__version__,
+
+    description='Graphical tool to convert a number from one base to another',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/NicolasBi/super_converter',
+
+    # Not to use with Python versions prior to 2.2.3 or 2.3
+    download_url='https://github.com/NicolasBi/super_converter/tarball/master',
+
+    # Author details
+    author='Bizzozzéro Nicolas',
+    author_email='nicolasbizzozzero@gmail.com',
+
+    # Choose your license
+    license='GPL-3.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: User Interfaces',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='GUI baseconversion binary decimal hexadecimal',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+
+    },
+
+    # Set to True if we use MANIFEST.in
+    include_package_data=True,
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'gui_scripts': [
+            'sconvgui=super_converter:main',
+        ],
+    },
 )
